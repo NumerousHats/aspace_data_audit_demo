7621d3373ddc3ecefa25b88e48d5237e4048ea41
Christy <christy.tomecek@yale.edu>
2017-11-03 14:39:39 -0400

    Add files via upload

--- queries/resource_dates.sql
+++ queries/resource_dates.sql
@@ -1,4 +1,5 @@
-SELECT date.resource_id, date.date_type_id, date.expression, date.begin, date.end, resource.repo_id, resource.ead_id, enumeration_value.value 
+SELECT date.resource_id, date.date_type_id, date.expression, date.begin, date.end, date.label_id, date.era_id, date.calendar_id, resource.repo_id, resource.title, resource.identifier, resource.ead_id, enumeration_value.value 
+, CONCAT('/repositories/', resource.repo_id, '/resources/', resource.id) AS Resource_URL
 FROM resource
 LEFT JOIN date on resource.id=date.resource_id 
 LEFT JOIN enumeration_value on enumeration_value.id=date.date_type_id
