205e6227bfc56965d455e4294059f32e7e10b587
Christy <christy.tomecek@yale.edu>
2017-11-03 14:43:56 -0400

    Add files via upload

--- queries/accession_dates.sql
+++ queries/accession_dates.sql
@@ -1,4 +1,5 @@
-SELECT date.accession_id, date.date_type_id, date.expression, date.begin, date.end, accession.repo_id, accession.identifier, enumeration_value.value 
+SELECT date.accession_id, date.date_type_id, date.expression, date.begin, date.end, date.era_id, date.calendar_id, date.label_id, accession.repo_id, accession.title, accession.identifier, enumeration_value.value 
+, CONCAT('/repositories/', accession.repo_id, '/accessions/', accession.id) AS Accession_URL
 FROM accession
 LEFT JOIN date on accession.id=date.accession_id
 LEFT JOIN enumeration_value on enumeration_value.id=date.date_type_id
